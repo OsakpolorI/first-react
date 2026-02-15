2efedacd37190007fc026e22fc6d2a9a944d0052
Osakpolor Idusuyi <osak.idusuyi@gmail.com>
2026-02-15 04:29:45 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,59 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import { Fragment } from 'react'
+import App from './App.jsx'
+
+/**
+ * Challenge:
+ * 
+ * - Add a `nav` > `ul` > `li` (x3). The 3 items should say:
+ *   "Pricing", "About", and "Contact"
+ */
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Page />
+  </StrictMode>
+)
+
+function Page() {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  )
+}
+
+function Header() {
+  return (
+    <header>
+      <img src="/src/assets/react.svg" width="40px" alt="React logo"/>
+    </header>
+  )
+}
+
+function MainContent() {
+  return (
+    <main>
+        <h1>Reasons I'm excited to learn React</h1>
+        <ol>
+          <li>I want to better my ability to learn.</li>
+          <li>I want to be more employable</li>
+          <li>I want to be able to make a living doing something I enjoy.</li>
+        </ol>
+      </main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 20xx Idusuyi development. All rights reserved</small>
+    </footer>
+  )
+}
+
+
